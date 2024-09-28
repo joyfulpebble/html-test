874d1e7e7e39ef440ef0df666df97387290f363b
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2024-09-28 19:22:15 +0300

    tooltips, tooltip for 1 point

---
+++ tooltip.js
@@ -0,0 +1,33 @@
+const targetElement = document.querySelector(".point");
+const tooltip = document.getElementById("tooltip");
+
+const element = document.getElementById("p_1_1");
+const style = window.getComputedStyle(element);
+const top = style.getPropertyValue("top").split("").splice(0, 3).join("");
+const left = style.getPropertyValue("left").split("").splice(0, 3).join("");
+
+let stable = false;
+
+targetElement.addEventListener("mouseenter", function () {
+  tooltip.style.top = `${+top - 140}px`;
+  tooltip.style.left = `${+left - 220}px`;
+  tooltip.style.display = "flex";
+});
+
+tooltip.addEventListener("mouseenter", function () {
+  stable = true;
+  tooltip.style.display = "flex";
+});
+
+tooltip.addEventListener("mouseleave", function () {
+  stable = false;
+  tooltip.style.display = "none";
+});
+
+targetElement.addEventListener("mouseleave", function () {
+  setTimeout(() => {
+    if (!stable) {
+      tooltip.style.display = "none";
+    }
+  }, 200);
+});
